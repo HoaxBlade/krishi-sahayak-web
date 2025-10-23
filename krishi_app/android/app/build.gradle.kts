bc6cce6d7eb9457a92e4ff4d89b5db79f07ca7a2
Ayush Ranjan <ranjanayush881@gmail.com>
2025-10-24 03:53:15 +0530

    some app improvements and desease detail feature added

--- krishi_app/android/app/build.gradle.kts
+++ krishi_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.krishi_app"
+    namespace = "com.krishisahayak.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
--- krishi_app/android/app/src/main/kotlin/com/krishisahayak/app/MainActivity.kt
+++ krishi_app/android/app/src/main/kotlin/com/krishisahayak/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.krishi_app
+package com.krishisahayak.app
 
 import io.flutter.embedding.android.FlutterActivity
 
